58254ed0cf9ee7f8040ebd8ed7a00034236d9ee2
Vibhutha Kumarage <vkartz@gmail.com>
2018-10-13 19:02:21 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+
+const server = http.createServer(function(req, res) {
+
+  // Parsing the URL with Query string
+  var parsedUrl = url.parse(req.url, true);
+  // console.log('Parsed with Query String', parsedUrl);
+
+  // var parsedUrlWQ = url.parse(req.url, false);
+  // console.log('Parsed without Query String', parsedUrlWQ);
+
+  // Get the pathName from the request
+  var path = parsedUrl.pathname;
+
+  // Get the query string as object
+  var queryString = parsedUrl.query;
+
+  // Get the headers from the request as an object
+  var headers = req.headers;
+
+  // Trimmed the path
+  var trimmedUrl = path.replace(/^\/+|\/+$/g, '');
+
+  var buffer = '';
+  var decoder = new StringDecoder('utf-8');
+
+  req.on('data', (data) => {
+    // buffer += data;
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+    // console.log('Headers :', headers);
+    // console.log('Buffer: ', buffer);
+    // res.end(trimmedUrl + ' Method :' + req.method.toUpperCase() + ' Query : ' + queryString + '\n');
+    // res.end(`${trimmedUrl}, Method: ${req.method.toUpperCase()}`);
+    
+    // Selecting the handler from the trimmedURL
+    var chosedHandler = typeof(router[trimmedUrl] !== 'undefined') ? router[trimmedUrl] : handlers.notFound;
+
+  });
+});
+
+server.listen(3000, function(){
+  console.log('Listening on:', 3000);
+});
+
+
+// Handlers object
+const handlers = {}
+
+// Add sampleRoute handler 
+handlers.sampleRoute = function(data, callback) {
+  callback(200, {'route': 'Got the route'});
+}
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404);
+}
+
+// Request router
+const router = {
+  'sampleRoute': handlers.sampleRoute
+}
